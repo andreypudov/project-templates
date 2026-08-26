fde4cdf3c05071eb1f158d10040cc2e6ff802c10
Andrey Pudov <andrey.pudov@supergroup.com>
2026-08-26 14:39:51 +0200

    ~ updated c and c++ basic templates

--- c-make-basic/program.c
+++ c-make-basic/program.c
@@ -3,12 +3,21 @@
 
 int main(int argc, char *argv[]) {
   int sum = 0;
-  for (int index = 1; index < argc; ++index) {
 
-    sum += atoi(argv[index]);
+  for (int index = 1; index < argc; ++index) {
+    char *end = nullptr;
+    long value = strtol(argv[index], &end, 10);
+
+    if (end == argv[index] || *end != '\0') {
+      fprintf(stderr, "Invalid integer: %s\n", argv[index]);
+
+      return 1;
+    }
+
+    sum += (int)value;
   }
 
   printf("Sum: %d\n", sum);
 
-  return (EXIT_SUCCESS);
+  return 0;
 }
--- cpp-make-basic/program.cc
+++ cpp-make-basic/program.cc
@@ -1,14 +1,23 @@
 #include <iostream>
-#include <string>
+#include <string_view>
+#include <charconv>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   int sum = 0;
 
   for (int index = 1; index < argc; ++index) {
-    sum += std::stoi(argv[index]);
+    std::string_view sv{argv[index]};
+    int value{};
+    auto result = std::from_chars(sv.data(), sv.data() + sv.size(), value);
+
+    if (result.ec != std::errc{} || result.ptr != sv.data() + sv.size()) {
+      std::cerr << "Invalid integer: " << sv << '\n';
+
+      return 1;
+    }
+
+    sum += value;
   }
 
   std::cout << "Sum: " << sum << '\n';
-
-  return 0;
 }
