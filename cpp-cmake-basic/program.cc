256c02e585ef7b3eecfca5e50215066a687e0e0f
Andrey Pudov <andrey.pudov@supergroup.com>
2026-08-31 16:58:20 +0200

    ~ added cpp-cmake-basic template

---
+++ cpp-cmake-basic/program.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string_view>
+#include <charconv>
+
+int main(int argc, char* argv[]) {
+  int sum = 0;
+
+  for (int index = 1; index < argc; ++index) {
+    std::string_view sv{argv[index]};
+    int value{};
+    auto result = std::from_chars(sv.data(), sv.data() + sv.size(), value);
+
+    if (result.ec != std::errc{} || result.ptr != sv.data() + sv.size()) {
+      std::cerr << "Invalid integer: " << sv << '\n';
+
+      return 1;
+    }
+
+    sum += value;
+  }
+
+  std::cout << "Sum: " << sum << '\n';
+}
